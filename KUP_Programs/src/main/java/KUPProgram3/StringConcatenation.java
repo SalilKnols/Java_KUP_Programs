8a21efd9fdcb529d2420458ecc31eed483a16778
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:17:07 +0530

    String Concatenation Program

---
+++ KUP_Programs/src/main/java/KUPProgram3/StringConcatenation.java
@@ -0,0 +1,12 @@
+package KUPProgram3;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String name = "John";
+        String message = "Hello, ";
+
+        String result = message + name;
+
+        System.out.println(result);
+    }
+}
