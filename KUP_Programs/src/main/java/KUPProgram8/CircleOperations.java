1ed5e5a51c9296c04b623e3b95c3f6290c711f56
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:19:42 +0530

    Program to perform Operations on a Circle

---
+++ KUP_Programs/src/main/java/KUPProgram8/CircleOperations.java
@@ -0,0 +1,13 @@
+package KUPProgram8;
+
+public class CircleOperations {
+    public static void main(String[] args) {
+        double radius = 3.0;
+
+        double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
+    }
+}
