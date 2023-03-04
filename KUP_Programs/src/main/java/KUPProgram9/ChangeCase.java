5ab8c00b3e18d50f44a566e7a73981ca202a9845
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:20:04 +0530

    Program to convert a string to uppercase

---
+++ KUP_Programs/src/main/java/KUPProgram9/ChangeCase.java
@@ -0,0 +1,11 @@
+package KUPProgram9;
+
+public class ChangeCase {
+    public static void main(String[] args) {
+        String message = "Hello";
+
+        String upperCaseMessage = message.toUpperCase();
+
+        System.out.println(upperCaseMessage);
+    }
+}
