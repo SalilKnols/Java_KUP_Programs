6012d6941265bedde8bb90afed8b2299e3921578
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:17:43 +0530

    If Else Program

---
+++ KUP_Programs/src/main/java/KUPProgram4/IfElse.java
@@ -0,0 +1,13 @@
+package KUPProgram4;
+
+public class IfElse {
+    public static void main(String[] args) {
+        boolean isRaining = true;
+
+        if (isRaining) {
+            System.out.println("Take an umbrella");
+        } else {
+            System.out.println("Enjoy the sunshine");
+        }
+    }
+}
