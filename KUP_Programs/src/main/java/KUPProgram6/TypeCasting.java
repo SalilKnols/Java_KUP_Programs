614bf307b2ea4d6cf0ddda8c914d58037fdac1e5
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:18:34 +0530

    Type Casting Program

---
+++ KUP_Programs/src/main/java/KUPProgram6/TypeCasting.java
@@ -0,0 +1,11 @@
+package KUPProgram6;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        double distance = 5.0;
+
+        int distanceAsInt = (int) distance;
+
+        System.out.println("Distance as integer: " + distanceAsInt);
+    }
+}
