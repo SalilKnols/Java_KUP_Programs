69ae178a6c72b3dce84c14799baf232a45bfa239
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:18:09 +0530

    While Loop Program

---
+++ KUP_Programs/src/main/java/KUPProgram5/Loop.java
@@ -0,0 +1,12 @@
+package KUPProgram5;
+
+public class Loop {
+    public static void main(String[] args) {
+        int x = 10;
+
+        while (x > 0) {
+            System.out.println(x);
+            x--;
+        }
+    }
+}
