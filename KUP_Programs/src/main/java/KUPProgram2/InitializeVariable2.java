d7306314936035090846cc7ab77a2ca68ae04dad
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:16:25 +0530

    Initialize Variable Second Program

---
+++ KUP_Programs/src/main/java/KUPProgram2/InitializeVariable2.java
@@ -0,0 +1,12 @@
+package KUPProgram2;
+
+public class InitializeVariable2 {
+    public static void main(String[] args) {
+        double num1 = 3.14;
+        double num2 = 6.28;
+
+        double sum = num1 + num2;
+
+        System.out.println("The sum of num1 and num2 is: " + sum);
+    }
+}
