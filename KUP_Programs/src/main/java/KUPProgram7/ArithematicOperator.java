26091ed489cde0a19ea5712bf5f908887e5a23a4
SalilKnols <salil.verma@knoldus.com>
2023-03-04 14:19:03 +0530

    Programs to perform arithematic operations

---
+++ KUP_Programs/src/main/java/KUPProgram7/ArithematicOperator.java
@@ -0,0 +1,18 @@
+package KUPProgram7;
+
+public class ArithematicOperator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
